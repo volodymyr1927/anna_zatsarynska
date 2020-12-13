4d8f8e3e6d892c12cba4579d0a5fb78332bd6239
Sergej <sergion4486@gmail.com>
2020-12-13 23:50:40 +0200

    added default temlate some canges at log in form

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,9 +6,16 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class DashboardController
+ *
+ * @IsGranted("ROLE_ADMIN", statusCode=404, message="Not found")
+ * @package App\Controller\Admin
+ */
 class DashboardController extends AbstractDashboardController
 {
     /**
