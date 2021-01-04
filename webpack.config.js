6428a15580f7d55e6f118d28574e5afa2b53506d
Sergej <sergion4486@gmail.com>
2021-01-04 22:43:34 +0200

    photo viewer and some style fixing

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('zoomwall', 'zoomwall.js/zoomwall.js')
+    .addStyleEntry('zoomwall_css', 'zoomwall.js/zoomwall.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
