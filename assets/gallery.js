a3485c53003d5b7d6faab9c24e2939d93bdcd68f
Voloshyn <volodymyr.voloshyn@corp.mailfire.io>
2021-05-15 23:32:32 +0300

    hide some part of navbar

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,6 +1,22 @@
 import Viewer from 'viewerjs'
 
 const gallery = new Viewer(document.getElementById('gallery'), {
-    'button': false
+    'button': false,
+    'title': false,
+    toolbar: {
+        zoomIn: 4,
+        zoomOut: 4,
+        oneToOne: 0,
+        reset: 0,
+        prev: 4,
+        play: {
+            show: 0,
+        },
+        next: 4,
+        rotateLeft: 0,
+        rotateRight: 0,
+        flipHorizontal: 0,
+        flipVertical: 0,
+    },
 });
 
